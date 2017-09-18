b5276b76563a51d6ee1fdf8c56ab33e1240a8c4a
io <micorreo.erick@gmail.com>
2017-09-18 14:36:54 -0400

    First commit

---
+++ public/js/firebaseConnection.js
@@ -0,0 +1,12 @@
+(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBE8FOLWgv3RiIiSHSZyYG7ykcGwncUOrs",
+    authDomain: "iowtf-ioerick.firebaseapp.com",
+    databaseURL: "https://iowtf-ioerick.firebaseio.com",
+    projectId: "iowtf-ioerick",
+    storageBucket: "iowtf-ioerick.appspot.com",
+    messagingSenderId: "736522305309"
+  };
+  firebase.initializeApp(config);
+} ());
